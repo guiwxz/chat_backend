7ef0b9af32d494184cdcc22940633f992d274e90
guiwxz <guiwxz@hotmail.com>
2021-07-11 03:38:08 -0300

    chore: criando Procfile

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import { Server } from 'socket.io';
-import a from 'socket.io';
 import routes from './src/routes.js';
 import connection from './src/socket/index.js';
 
